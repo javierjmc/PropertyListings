29dfd5b9924ade0d4e82c55a1315127e020d1c52
Javier Mendonça <javier.jmc@gmail.com>
2018-06-29 02:10:33 +0200

    First draft of displaying a list of listings

--- app/src/main/java/se/hemnet/property/list/ListingViewHolder.kt
+++ app/src/main/java/se/hemnet/property/list/ListingViewHolder.kt
@@ -2,19 +2,38 @@ package se.hemnet.property.list
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.bumptech.glide.Glide
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.listing_item.*
+import se.hemnet.property.R
+import se.hemnet.property.extensions.getFormattedString
+import se.hemnet.property.extensions.getPluralString
 import se.hemnet.property.model.Listing
+import kotlinx.android.synthetic.main.listing_item.area as dimensions
 
 /**
  * [ViewHolder] for each [Listing] on the list.
  * */
-class ListingViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class ListingViewHolder(override val containerView: View, val onClick: (Listing) -> Unit) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
     /**
      * Binds the given listing
      *
      * @param listing [Listing] to be bound.
      * */
-    fun bind(listing: Listing) {
+    fun bind(listing: Listing): Unit = with(listing) {
+        address.text = streetAddress
+        localArea.text = getFormattedString(R.string.area_municipality, area, livingArea)
+        price.text = askingPrice
+        dimensions.text = getFormattedString(R.string.living_area, livingArea)
+        rooms.text = getFormattedString(R.string.rooms, numberOfRooms)
+        fee.text = monthlyFee
+        days.text = getPluralString(R.plurals.days, daysOnHemnet, daysOnHemnet)
 
+        Glide.with(containerView.context)
+                .load(thumbnail)
+                .into(image)
+
+        containerView.setOnClickListener { onClick(this) }
     }
 }
