7290fbf213702278147a9b869016c721c76b5850
Javier Mendonça <javier.jmc@gmail.com>
2018-06-29 22:54:00 +0200

    Adding new viewstate to the viewmodel

--- app/src/main/java/se/hemnet/property/viewmodel/ListingViewModel.kt
+++ app/src/main/java/se/hemnet/property/viewmodel/ListingViewModel.kt
@@ -1,6 +1,6 @@
 package se.hemnet.property.viewmodel
 
-import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import se.hemnet.property.model.Listing
 import se.hemnet.property.repository.ListingsRepository
@@ -11,5 +11,19 @@ import se.hemnet.property.repository.ListingsRepository
  * @param listingsRepository [ListingsRepository] to pull the listings from.
  * */
 class ListingViewModel(listingsRepository: ListingsRepository) : ViewModel() {
-    val listings: LiveData<List<Listing>> = listingsRepository.getListings()
+    val viewState: MutableLiveData<ViewState> = MutableLiveData()
+
+    init {
+        with(viewState) {
+            value = ViewState.Loading
+
+            val listings = listingsRepository.getListings()
+
+            value = if (listings.value == null) {
+                ViewState.Error("Problem loading data")
+            } else {
+                ViewState.Loaded(listingsRepository.getListings().value)
+            }
+        }
+    }
 }
